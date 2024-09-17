eded47f3aa5b98c01b34bf30d48de33a0bbe5623
Hernan Dario Castro <3psdariocastro@gmail.com>
2024-09-17 20:18:06 -0300

    Add files via upload

---
+++ restinicial-main/src/main/java/com/example/inicial1/entities/Persona.java
@@ -0,0 +1,30 @@
+package com.example.inicial1.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+
+import java.io.Serializable;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Audited
+
+
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+}
