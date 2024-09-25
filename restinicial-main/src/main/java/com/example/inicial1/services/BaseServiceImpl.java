f1f76eca69fab3d25a9f3517679bdbfda0c0794a
Hernan <3psdariocastro@gmail.com>
2024-09-25 16:35:13 -0300

    Added query and pagination

--- restinicial-main/src/main/java/com/example/inicial1/services/BaseServiceImpl.java
+++ restinicial-main/src/main/java/com/example/inicial1/services/BaseServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.inicial1.services;
 import com.example.inicial1.entities.Base;
 import com.example.inicial1.repositories.BaseRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 import java.util.List;
@@ -76,4 +78,16 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
             throw  new Exception(e.getMessage());
         }
     }
+
+    @Override
+    @Transactional
+    public Page<E> findAll(Pageable pageable) throws Exception{
+        try{
+            Page<E> entities = baseRepository.findAll(pageable);
+            return entities;
+        }catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
 }
